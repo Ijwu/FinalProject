7a8b1e3e7a53aa20ef9fdb00df15da6912d4eb87
Ijwu <hmfarran1@hawkmail.hfcc.edu>
2014-08-05 11:06:57 -0400

    Added Data Model for Code First to new database. Added Migration support. Enabled Automatic Migrations.

--- FinalProject.Data/FinalProjectModel.cs
+++ FinalProject.Data/FinalProjectModel.cs
@@ -1,9 +1,12 @@
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Data;
+
 namespace FinalProject.Data
 {
-    using System;
-    using System.Data.Entity;
-    using System.Linq;
-
     public class FinalProjectModel : DbContext
     {
         // Your context has been configured to use a 'FinalProjectModel' connection string from your application's 
@@ -20,7 +23,10 @@ namespace FinalProject.Data
         // Add a DbSet for each entity type that you want to include in your model. For more information 
         // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
 
-        //public virtual DbSet<MyEntity> MyEntities { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Post> Posts { get; set; }
+        public virtual DbSet<BudgetBox> BudgetBoxs { get; set; }
+        public virtual DbSet<Transaction> Transactions { get; set; }
     }
 
     //public class MyEntity
@@ -28,4 +34,70 @@ namespace FinalProject.Data
     //    public int Id { get; set; }
     //    public string Name { get; set; }
     //}
+
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(4), MaxLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+        public string ProfilePhotoPath { get; set; }
+
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public virtual User User { get; set; }
+    }
+
+    public class BudgetBox
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public int StartingAmount { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual ICollection<Transaction> Transactions { get; set; } 
+    }
+
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+        public int BudgetBoxId { get; set; }
+
+        [Required]
+        public int UnitPrice { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        public virtual BudgetBox BudgetBox { get; set; }
+    }
 }
