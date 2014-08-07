927187f88e40ef2e889b069866b1704f7cdd9e3f
Ijwu <hmfarran1@hawkmail.hfcc.edu>
2014-08-07 15:55:38 -0400

    Added UserManager to UserController

--- FinalProject/Controllers/UserController.cs
+++ FinalProject/Controllers/UserController.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace FinalProject.Controllers
 {
     public class UserController : Controller
     {
+        private ApplicationUserManager _userManager;
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
         // GET: User
         public ActionResult Index()
         {
